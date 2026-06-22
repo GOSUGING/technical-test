fbb5120c62c219feab9c54d4aeb0d35acd596b54
Technical Test <test@example.com>
2026-06-22 12:04:26 -0400

    fix minor error

--- src/test/java/com/technicaltest/logic/ListProcessorTest.java
+++ src/test/java/com/technicaltest/logic/ListProcessorTest.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 /**
